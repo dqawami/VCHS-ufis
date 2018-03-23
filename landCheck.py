2644253300c1807bfe81ce2456bd37aed154b169
dqawami <daryan.qawami@warriorlife.net>
2018-03-23 23:56:20 +0000

    Prints climb in order to find the maximum climb issue in a static position

--- landCheck.py
+++ landCheck.py
@@ -2,6 +2,8 @@
 
 from gps3 import gps3
 import subprocess
+import sys
+from time import sleep
 
 #flightMode is the current stage of flight
 #l = launchpad
@@ -14,7 +16,9 @@ flightMode = 'l'
 #climb is the vertical velocity (positive value for up, negative value for down)
 climb = 0
 #climbOffset is the climb error value
-climbOffset = 0
+#climbOffset = 0.18
+#maxClimb is the largest climb value found
+maxClimb = 0
 
 #Command line commands to set up gps
 subprocess.call(["sudo", "systemctl", "stop", "gpsd.socket"])
@@ -31,21 +35,47 @@ data_stream = gps3.DataStream()
 gps_socket.connect()
 gps_socket.watch()
 
-#Check for new data
-for new_data in gps_socket:
-	if new_data:
-		#Unpack data for data access
-		data_stream.unpack(new_data)
-		#Assign altitude and climb new values
-		#altitude = data_stream.TPV['alt']
-		climb = data_stream.TPV['climb']
-		climbOffset = data_stream.TPV['epc']
-		#If the rocket is moving upward from the launchpad (climb is positive), flightMode is in ascent
-		if (climb - climbOffset > 0 and flightMode == 'l'):
-			flightMode = 'a'
-		#If the rocket is moving downward while in ascent mode (climb is negative), flightMode is in descent
-		elif (climb < 0 and flightMode == 'a'):
-			flightMode = 'd'
-		#If the rocket has stopped moving while in descent mode, flightMode is in crash
-		elif ((climb - climbOffset == 0 or climb + climbOffset == 0) and flightMode == 'd'):
-			flightMode = 'c'
+try:
+	#Check for new data
+	for new_data in gps_socket:
+		if new_data:
+			#Unpack data for data access
+			data_stream.unpack(new_data)
+			#Assign altitude and climb new values
+			#altitude = data_stream.TPV['alt']
+			#Wait until GPS has lock
+#			print('Climb error = ',data_stream.TPV['epc']
+			if ((data_stream.TPV['climb'] == 'n/a')):
+				print("GPS does not have lock")
+				continue
+#
+			climb = float(data_stream.TPV['climb'])
+			print ("Climb = ", climb)
+			if (abs(maxClimb) < abs(climb)):
+				maxClimb = climb
+#			climbOffset = int(data_stream.TPV['epc'])
+#			#If the rocket is moving upward from the launchpad (climb is positive), flightMode is in ascent
+#			if (climb - climbOffset > 0 and flightMode == 'l'):
+#				flightMode = 'a'
+#			#If the rocket is moving downward while in ascent mode (climb is negative), flightMode is in descent
+#			elif (climb < 0 and flightMode == 'a'):
+#				flightMode = 'd'
+#			#If the rocket has stopped moving while in descent mode, flightMode is in crash
+#			elif ((climb - climbOffset == 0 or climb + climbOffset == 0) and flightMode == 'd'):
+#				flightMode = 'c'
+#			#Print statements for testing
+#			print(climb)
+#			print(climbOffset)
+#			print(flightMode)
+#			time.sleep(.1)
+
+except StopIteration:
+	print ("Max Climb = ", maxClimb)
+	data_stream = None
+	gps_socket = None
+	print ("GPSD has terminated")
+
+except KeyboardInterrupt:
+	print ("Max Climb = ", maxClimb)
+	print("We killed it")
+	sys.exit()
